f0c2b7cc852e18a13c4ac1976e3f3da0368f3275
Phingaz <piinoya@gmail.com>
2023-08-19 21:06:35 -0300

    updates

--- src/component/Steps/Four.jsx
+++ src/component/Steps/Four.jsx
@@ -2,13 +2,75 @@ import "./Four.scss";
 import { Wrapper } from "../Utility/Wrapper";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
 
 export const Four = () => {
   const plan = useSelector((state) => state.userForm.plan);
   const price = useSelector((state) => state.userForm.pricing);
   const addons = useSelector((state) => state.userForm.addOns);
 
+  const [total, setTotal] = useState(0);
+  const [finalPlan, setFinalPlan] = useState(0);
+
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const online = addons.online
+      ? plan.isYearly
+        ? price.online.yearly
+        : price.online.monthly
+      : 0;
+    const storage = addons.storage
+      ? plan.isYearly
+        ? price.storage.yearly
+        : price.storage.monthly
+      : 0;
+    const profile = addons.profile
+      ? plan.isYearly
+        ? price.profile.yearly
+        : price.profile.monthly
+      : 0;
+
+    switch (plan.selectedPlan) {
+      case "arcade":
+        plan.isYearly
+          ? setFinalPlan(price.arcade.yearly)
+          : setFinalPlan(price.arcade.monthly);
+        break;
+      case "advanced":
+        plan.isYearly
+          ? setFinalPlan(price.advanced.yearly)
+          : setFinalPlan(price.advanced.monthly);
+        break;
+      case "pro":
+        plan.isYearly
+          ? setFinalPlan(price.pro.yearly)
+          : setFinalPlan(price.pro.monthly);
+        break;
+      default:
+        break;
+    }
+    setTotal(online + storage + profile + finalPlan);
+  }, [
+    addons.online,
+    addons.profile,
+    addons.storage,
+    finalPlan,
+    plan.isYearly,
+    plan.selectedPlan,
+    price.advanced.monthly,
+    price.advanced.yearly,
+    price.arcade.monthly,
+    price.arcade.yearly,
+    price.online.monthly,
+    price.online.yearly,
+    price.pro.monthly,
+    price.pro.yearly,
+    price.profile.monthly,
+    price.profile.yearly,
+    price.storage.monthly,
+    price.storage.yearly,
+  ]);
 
   return (
     <Wrapper>
@@ -23,14 +85,10 @@ export const Four = () => {
                 <h2>
                   {plan.selectedPlan} ({plan.isYearly ? "Yearly" : "Monthly"})
                 </h2>
-                <a>Change</a>
+                <a onClick={() => navigate("/")}>Change</a>
               </div>
               <p className="month">
-                {plan.isYearly ? (
-                  <>${price.arcade.yearly}/yr</>
-                ) : (
-                  <>${price.arcade.monthly}/mo</>
-                )}
+                {plan.isYearly ? <>${finalPlan}/yr</> : <>${finalPlan}/mo</>}
               </p>
             </div>
             {addons.online && (
@@ -66,9 +124,9 @@ export const Four = () => {
             <div className="category total">
               <p>Total ({plan.isYearly ? "per year" : "per month"})</p>
               {plan.isYearly ? (
-                <p className="month">+${price.profile.yearly}/yr</p>
+                <p className="month">+${total}/yr</p>
               ) : (
-                <p className="month">+${price.profile.monthly}/mo</p>
+                <p className="month">+${total}/mo</p>
               )}
             </div>
           </div>
